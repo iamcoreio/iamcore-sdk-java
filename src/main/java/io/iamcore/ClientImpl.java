cbd586a815cc6f51162a1b971a6717ca152bfa34
Andrew Pasika <apasika@kaaiot.io>
2024-01-17 08:50:23 +0000

    AGILE-4921: java 21

--- src/main/java/io/iamcore/ClientImpl.java
+++ src/main/java/io/iamcore/ClientImpl.java
@@ -24,7 +24,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 public class ClientImpl implements Client {
 
