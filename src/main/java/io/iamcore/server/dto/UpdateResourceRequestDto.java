a901d21cc37231b6c51bbc4c5cc68c259cd54fa8
Evhen Malysh <emalysh@kaaiot.io>
2025-07-07 12:39:17 +0000

    feat(resource): Implement resource update in client SDK

--- src/main/java/io/iamcore/server/dto/UpdateResourceRequestDto.java
+++ src/main/java/io/iamcore/server/dto/UpdateResourceRequestDto.java
@@ -14,9 +14,10 @@ public class UpdateResourceRequestDto {
   public JSONObject toJson() {
     JSONObject json = new JSONObject();
     if (this.poolIds != null) {
-      json.put("poolIds", poolIds);
+      json.put("poolIDs", poolIds);
     }
 
     return json;
   }
 }
+
